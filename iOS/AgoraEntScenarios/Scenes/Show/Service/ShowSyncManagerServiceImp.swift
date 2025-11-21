032e9befa98b5e9431a0cdf034396f94b3db6a6f
zoucheng <zoucheng@agora.io>
2025-11-21 18:24:14 +0800

    [FEAT] FU version live 1h not expire

--- iOS/AgoraEntScenarios/Scenes/Show/Service/ShowSyncManagerServiceImp.swift
+++ iOS/AgoraEntScenarios/Scenes/Show/Service/ShowSyncManagerServiceImp.swift
@@ -10,8 +10,8 @@ import AgoraCommon
 import RTMSyncManager
 import AgoraRtmKit
 
-private let kSceneId = "scene_show_6.0.0"
-private let kRoomPresenceChannelName = "scene_show_6_0_0_9999999"
+private let kSceneId = "scene_show_6.9.9"
+private let kRoomPresenceChannelName = "scene_show_6_9_9_9999999"
 
 private func agoraPrint(_ message: String) {
     ShowLogger.info(message, context: "Service")
@@ -198,7 +198,7 @@ extension ShowSyncManagerServiceImp {
                   let scene = self.syncManager.getScene(channelName: roomId) else { return }
             
             let duration = scene.getRoomDuration()
-            let expiredDuration = (AppContext.shared.sceneConfig?.show ?? 10 * 60) * 1000
+            let expiredDuration = (4800) * 1000
             if duration >= expiredDuration {
                 self.stopCheckExpireTimer(roomId: roomId)
                 
